6487c162dd86b542b71604f63c342a75f65e687d
Daniel Poobalan <daniel.poobalan@gmail.com>
2018-07-30 21:16:43 +0800

    Update User.js

--- app/server/models/User.js
+++ app/server/models/User.js
@@ -28,7 +28,7 @@ var profile = {
   graduationYear: {
     type: String,
     enum: {
-      values: '2016 2017 2018 2019'.split(' '),
+      values: '2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019'.split(' '),
     }
   },
 
